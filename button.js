3a0818dbf1cb264ae39c8f81e6a095a7fd13d84d
prabinLC <prabinlc80@gmail.com>
2024-08-25 18:56:59 -0500

    This is just the demo

---
+++ button.js
@@ -0,0 +1,11 @@
+"use strict";
+const body = document.getElementsByTagName("body")[0];
+
+const button1 = document.querySelector(".button1");
+const button2 = document.querySelector(".button2");
+const button3 = document.querySelector(".button3");
+const button4 = document.querySelector(".button4");
+
+function setBackgroungColor(colors) {
+  body.style.backgroundColor = colors;
+}
